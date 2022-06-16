68c102e12bcb724ddc43be4e9108aed08bc89da9
shehan <shehansugathapala035@gmail.com>
2022-06-16 14:37:10 +0530

    Update Connection

--- src/controllers/ConnectDB.java
+++ src/controllers/ConnectDB.java
@@ -20,10 +20,10 @@ public class ConnectDB {
     public static Connection getConn() {
         if (conn == null) {
             try {
-                serverIP = "localhost";
+                serverIP = "192.168.8.101";
                 port = "3306";
                 database = "time_triker";
-                username = "root";
+                username = "remote_user";
                 password = "9tQAo^VMneM@";
                 Class.forName("com.mysql.cj.jdbc.Driver");
                 conn = DriverManager.getConnection("jdbc:mysql://" + serverIP + ":" + port + "/" + database + "?autoReconnect=true&useSSL=false", "" + username + "", "" + password + "");
